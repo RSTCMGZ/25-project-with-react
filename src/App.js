b12eb2f00d81ccaf7dc0b76db5cd169b70a00890
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-01-10 02:09:29 +0300

    start-slider-react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
+import ImageSlider from './components/img-slider/ImageSlider';
 // import Accordian from './components/accordion';
-import StartRating from './components/start-rating/StartRating';
+// import StartRating from './components/start-rating/StartRating';
 
 function App() {
   return (
     <div className="App">
       {/* Accordian component */}
       {/* <Accordian /> */}
-      <StartRating noOfStarts={10} />
+      {/* <StartRating noOfStarts={10} /> */}
+      <ImageSlider url={"https://picsum.photos/v2/list"} page={'1'} limit={'10'} />
     </div>
   );
 }
---
+++ src/components/img-slider/ImageSlider.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react'
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+
+import "./style.css"
+
+
+
+function ImageSlider({ url, limit = 5, page = 1 }) {
+    const [images, SetImages] = useState([])
+    const [currentSlide, setCurrentSlide] = useState(0)
+    const [errorMsg, setErrorMsg] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    async function fetchImages(getUrl) {
+        try {
+            setLoading(true)
+            const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
+            const data = await response.json();
+
+            if (data) {
+                SetImages(data)
+                setLoading(false)
+            }
+        } catch (e) {
+            setErrorMsg(e.message)
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        if (url !== "") fetchImages(url);
+    }, [url]);
+
+    console.log(images);
+    if (loading) {
+        return <div>Loading data ! Please wait</div>
+    }
+
+    if (errorMsg !== null) {
+        return <div>Error occured ! {errorMsg}</div>
+    }
+
+    return (
+        <div className='container'>
+            <BsArrowLeftCircleFill className='arrow arrow-left' />
+            {
+                images && images.length ?
+                    images.map(imageItem => (
+                        <img
+                            key={imageItem.id}
+                            alt={imageItem.download_url}
+                            src={imageItem.download_url}
+                            className='current-image'
+                        />
+                    ))
+                    : null
+            }
+            <BsArrowRightCircleFill className='arrow arrow-right' />
+            <span className='circle-indicators'>
+                {
+                    images && images.length ?
+                        images.map((_, index) => (
+                            <button key={index} className='current-indicator'>
+                            </button>
+                        ))
+                        : null}
+
+            </span>
+        </div>
+    )
+}
+
+export default ImageSlider
